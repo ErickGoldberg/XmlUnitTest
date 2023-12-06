af78ba90c4ed71be16da4cc4aa495ed559fabe01
Erick Goldberg <erick_goldberg@hotmail.com>
2023-12-05 22:26:47 -0300

    Including testes of the challenge

--- XmlDevTest.cs
+++ XmlDevTest.cs
@@ -1,38 +1,108 @@
 using DevEnsinaDevXml;
+using System.Runtime.ExceptionServices;
+using System.Xml;
 
 namespace XmlUnitTest
 {
     [TestFixture]
     public class XmlDevTest
     {
-        private string Xml;
+        XmlDev xmlDev;
+        private string _xmlCompleted;
+        private string _xmlInvalid;
+        private string _xmlWithoutCnpjEmit;
+        private string _xmlWithoutNumber;
 
         [SetUp]
         public void SetUp()
         {
-            //Leitura do Arquivo
-            var XmlCompleted = File.ReadAllText(@"C:\Users\erick\OneDrive\Área de Trabalho\Programação\Testes .NET\XmlUnitTest\XmlUnitTest\Files\XmlCompleted.txt");
-            var XmlInvalid = File.ReadAllText(@"C:\Users\erick\OneDrive\Área de Trabalho\Programação\Testes .NET\XmlUnitTest\XmlUnitTest\Files\XmlInvalid.txt");
-            var XmlWithoutCnpjEmit = File.ReadAllText(@"C:\Users\erick\OneDrive\Área de Trabalho\Programação\Testes .NET\XmlUnitTest\XmlUnitTest\Files\XmlWithoutCnpjEmit.txt");
-            var XmlWithoutNumber = File.ReadAllText(@"C:\Users\erick\OneDrive\Área de Trabalho\Programação\Testes .NET\XmlUnitTest\XmlUnitTest\Files\XmlWithoutNumber.txt");
+            // Arrange
+            xmlDev = new XmlDev();
+
+            _xmlCompleted = File.ReadAllText(@"C:\Users\erick\OneDrive\Área de Trabalho\Programação\Testes .NET\XmlUnitTest\XmlUnitTest\Files\XmlCompleted.txt");
+            _xmlInvalid = File.ReadAllText(@"C:\Users\erick\OneDrive\Área de Trabalho\Programação\Testes .NET\XmlUnitTest\XmlUnitTest\Files\XmlInvalid.txt");
+            _xmlWithoutCnpjEmit = File.ReadAllText(@"C:\Users\erick\OneDrive\Área de Trabalho\Programação\Testes .NET\XmlUnitTest\XmlUnitTest\Files\XmlWithoutCnpjEmit.txt");
+            _xmlWithoutNumber = File.ReadAllText(@"C:\Users\erick\OneDrive\Área de Trabalho\Programação\Testes .NET\XmlUnitTest\XmlUnitTest\Files\XmlWithoutNumber.txt");
+        }
+
+        #region TestExceptions
+
+        [Test]
+        [Category("TestExceptions")]
+        public void ReadXml_WhenLoad_ThrowInvalidDataExceptionInXmlGeneral()
+        {
+            Assert.Throws<InvalidDataException>(() => xmlDev.ReadXml(_xmlInvalid));
         }
 
         [Test]
-        public void IsXmlValidTest()
+        [Category("TestExceptions")]
+        public void ReadXml_WhenLoad_ThrowInvalidDataExceptionInCnpjEmit()
         {
-            Assert.Pass();
+            Assert.Throws<InvalidDataException>(() => xmlDev.ReadXml(_xmlWithoutCnpjEmit));
         }
 
         [Test]
-        public void XmlHasAllRequiredInfoTest()
+        [Category("TestExceptions")]
+        public void ReadXml_WhenLoad_ThrowFormatExceptionInNumXml()
         {
-            Assert.Pass();
+            Assert.Throws<FormatException>(() => xmlDev.ReadXml(_xmlWithoutNumber));
+        }
+
+        #endregion 
+
+        #region TestRequiredInfo
+
+        [Test]
+        [Category("TestRequiredInfo")]
+        public void ReadXml_WhenLoad_ReturnsAValidXmlNumber()
+        {
+            // Act
+            var xml = xmlDev.ReadXml(_xmlCompleted);
+
+            // Assert
+            Assert.Greater(xml?.NumXml, 0);
         }
 
         [Test]
-        public void TestXmlExceptions()
+        [Category("TestRequiredInfo")]
+        public void ReadXml_WhenLoad_ReturnsAValidCnpjEmit()
         {
-            Assert.Pass();
+            // Act
+            var xml = xmlDev.ReadXml(_xmlCompleted);
+
+            // Assert
+            Assert.That(xml?.CnpjEmit, Has.Length.EqualTo(14));
         }
+
+        [Test]
+        [Category("TestRequiredInfo")]
+        public void ReadXml_WhenLoad_ReturnsAValidCnpjDest()
+        {
+            // Act
+            var xml = xmlDev.ReadXml(_xmlCompleted);
+
+            // Assert
+            Assert.That(xml?.CnpjDest, Has.Length.EqualTo(14));
+        }
+
+
+        /* PROD CODE */
+
+        [Test]
+        [Ignore("Example of production code")]
+        [Category("TestRequiredInfo")]
+        public void ReadXml_WhenLoad_ReturnsValidFields()
+        {
+            // Act
+            var xml = xmlDev.ReadXml(_xmlCompleted);
+
+            // Assert
+            Assert.Greater(xml?.NumXml, 0);
+            Assert.That(xml?.CnpjEmit, Has.Length.EqualTo(14));
+            Assert.That(xml?.CnpjDest, Has.Length.EqualTo(14));
+        }
+
+        #endregion
+
     }
 }
